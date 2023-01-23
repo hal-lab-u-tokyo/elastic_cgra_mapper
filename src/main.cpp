aec4ea87017316c6b919fe68ec0b4631e9b3719f
msBRF65 <msaito6525@gmail.com>
2023-01-23 22:28:38 +0900

    add: link io and entity lib to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
-#include <boost/array.hpp>
 #include <gurobi_c++.h>
 
-int main(){
-    std::cout << "Hello world!" << std::endl;
-    boost::array<int, 4> a;
+#include <boost/array.hpp>
+
+int main() {
+  std::cout << "Hello world!" << std::endl;
+  boost::array<int, 4> a;
 }
