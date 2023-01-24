179a98be4eeae8cb34c0b3570758a1c91fc8c679
msBRF65 <msaito6525@gmail.com>
2023-01-24 19:52:54 +0900

    add: interface for creating MRRG from config data

--- entity/include/entity/mrrg.hpp
+++ entity/include/entity/mrrg.hpp
@@ -5,17 +5,53 @@
 
 namespace entity {
 struct MRRGNodeProperty {
-  bool is_elastic;
   bool is_memory_accessible;
+
   int local_reg_size;
   int context_size;
-  std::pair<int, int> position;
-  std::vector<OpType> support_op;
+
+  std::pair<int, int> position_id;
+  int context_id;
+
+  std::vector<OpType> supported_operations;
 };
 
 struct MRRGEdgeProperty {};
 
-struct MRRGGraphProperty {};
+enum MRRGCGRAType {
+  kDefault,
+  kElastic,
+};
+
+struct MRRGGraphProperty {
+  MRRGCGRAType mrrg_type;
+};
+
+enum MRRGMemoryIOType {
+  kAll,
+};
+
+enum MRRGNetworkType {
+  kOrthogonal,
+  kDiagonal,
+};
+
+struct MRRGConfig {
+  int column;
+  int row;
+  MRRGMemoryIOType memory_io;
+  MRRGCGRAType cgra_type;
+  MRRGNetworkType network_type;
+  int local_reg_size;
+  int contest_size;
+};
+
+MRRGCGRAType MRRGCGRATypeFromString(std::string cgra_type_string);
+std::string MRRGCGRATypeToString(MRRGCGRAType cgra_type);
+MRRGMemoryIOType MRRGMemoryIOTypeFromString(std::string memory_io_type_string);
+std::string MRRGMemoryIoTypeToString(MRRGMemoryIOType memory_io_type);
+MRRGNetworkType MRRGNetworkTypeFromString(std::string network_type_string);
+std::string MRRGNetworkTypeToString(MRRGNetworkType network_type);
 
 typedef BaseGraph<MRRGNodeProperty, MRRGEdgeProperty, MRRGGraphProperty>
     MRRGGraph;
@@ -26,5 +62,6 @@ class MRRG : public BaseGraphClass<MRRGNodeProperty, MRRGEdgeProperty,
   using BaseGraphClass<MRRGNodeProperty, MRRGEdgeProperty,
                        MRRGGraphProperty>::BaseGraphClass;
   MRRG(MRRGGraph mrrg_graph);
+  MRRG(MRRGConfig mrrg_config);
 };
 }  // namespace entity
