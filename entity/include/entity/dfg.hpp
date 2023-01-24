dad7ba2e617a65ab60d34e0711f8e7362f9bb00d
msBRF65 <msaito6525@gmail.com>
2023-01-24 15:51:11 +0900

    chore: format

--- entity/include/entity/dfg.hpp
+++ entity/include/entity/dfg.hpp
@@ -3,28 +3,28 @@
 #include <entity/graph.hpp>
 
 namespace entity {
-  enum OpType { ADD, SUB, MUL, DIV, CONST, LOAD, OUTPUT };
-  std::string OpTypeToString(OpType op);
-  OpType OpTypeFromString(std::string op_string);
+enum OpType { ADD, SUB, MUL, DIV, CONST, LOAD, OUTPUT };
+std::string OpTypeToString(OpType op);
+OpType OpTypeFromString(std::string op_string);
 
-  struct DFGNodeProperty {
-    OpType op;
-    std::string op_str;
-  };
+struct DFGNodeProperty {
+  OpType op;
+  std::string op_str;
+};
 
-  struct DFGEdgeProperty {
-    int operand;    
-  };
+struct DFGEdgeProperty {
+  int operand;
+};
 
-  struct DFGGraphProperty {};
+struct DFGGraphProperty {};
 
-  typedef BaseGraph<DFGNodeProperty, DFGEdgeProperty, DFGGraphProperty> DFGGraph;
+typedef BaseGraph<DFGNodeProperty, DFGEdgeProperty, DFGGraphProperty> DFGGraph;
 
-  class DFG: public BaseGraphClass<DFGNodeProperty, DFGEdgeProperty,
-    DFGGraphProperty> {
-  public:
-    using BaseGraphClass<DFGNodeProperty, DFGEdgeProperty,
-      DFGGraphProperty>::BaseGraphClass;
-    DFG(DFGGraph dfg_graph);
-  };
+class DFG : public BaseGraphClass<DFGNodeProperty, DFGEdgeProperty,
+                                  DFGGraphProperty> {
+ public:
+  using BaseGraphClass<DFGNodeProperty, DFGEdgeProperty,
+                       DFGGraphProperty>::BaseGraphClass;
+  DFG(DFGGraph dfg_graph);
+};
 }  // namespace entity
