ac1b29be07ac0a7d5b4f07beb434a02d95ed50ab
msBRF65 <msaito6525@gmail.com>
2023-01-24 15:51:24 +0900

    add: mrrg entity

--- entity/include/entity/mrrg.hpp
+++ entity/include/entity/mrrg.hpp
@@ -1,8 +1,30 @@
+#pragma once
+
+#include <entity/dfg.hpp>
 #include <entity/graph.hpp>
 
 namespace entity {
-class DFG : public BaseGraphClass {
- public:
-  using BaseGraphClass::BaseGraphClass;
+struct MRRGNodeProperty {
+  bool is_elastic;
+  bool is_memory_accessible;
+  int local_reg_size;
+  int context_size;
+  std::pair<int, int> position;
+  std::vector<OpType> support_op;
 };
-}
+
+struct MRRGEdgeProperty {};
+
+struct MRRGGraphProperty {};
+
+typedef BaseGraph<MRRGNodeProperty, MRRGEdgeProperty, MRRGGraphProperty>
+    MRRGGraph;
+
+class MRRG : public BaseGraphClass<MRRGNodeProperty, MRRGEdgeProperty,
+                                   MRRGGraphProperty> {
+ public:
+  using BaseGraphClass<MRRGNodeProperty, MRRGEdgeProperty,
+                       MRRGGraphProperty>::BaseGraphClass;
+  MRRG(MRRGGraph mrrg_graph);
+};
+}  // namespace entity
--- entity/src/mrrg.cpp
+++ entity/src/mrrg.cpp
@@ -1,1 +1,5 @@
 #include <entity/mrrg.hpp>
+
+entity::MRRG::MRRG(entity::MRRGGraph mrrg_graph)
+    : entity::BaseGraphClass<entity::MRRGNodeProperty, entity::MRRGEdgeProperty,
+                             entity::MRRGGraphProperty>(mrrg_graph){};
