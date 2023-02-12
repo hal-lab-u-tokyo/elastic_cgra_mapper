a919f533427e546125c601beb4fbe6e3351086f6
msBRF65 <msaito6525@gmail.com>
2023-02-12 21:47:38 +0900

    add: hash func for unordered_map in mapping

--- entity/include/entity/architecture.hpp
+++ entity/include/entity/architecture.hpp
@@ -10,6 +10,19 @@ struct ConfigId {
   int row_id;
   int column_id;
   int context_id;
+
+  bool operator==(const ConfigId& config_id) const {
+    return row_id == config_id.row_id && column_id == config_id.column_id &&
+           context_id == config_id.context_id;
+  }
+};
+
+struct HashConfigId {
+ public:
+  size_t operator()(const ConfigId& config_id) const {
+    return config_id.row_id * 1000 + config_id.column_id * 1000 +
+           config_id.context_id * 1000;
+  }
 };
 
 struct CGRAConfig {
