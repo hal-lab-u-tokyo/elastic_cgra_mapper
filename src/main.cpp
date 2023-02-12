1571d51c0fb738ff93e5fd30a04c47ad2331c25d
msBRF65 <msaito6525@gmail.com>
2023-02-12 21:49:35 +0900

    add: test code to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <gurobi_c++.h>
 
 #include <boost/array.hpp>
+#include <io/architecture_io.hpp>
+
+#include <boost/graph/adjacency_list.hpp>
 
 int main() {
   std::cout << "Hello world!" << std::endl;
   boost::array<int, 4> a;
+
+  // test: boost adjacent vertices
+  boost::adjacency_list<> g;
+  boost::adjacency_list<>::vertex_descriptor v1 = boost::add_vertex(g);
+  boost::adjacency_list<>::vertex_descriptor v2 = boost::add_vertex(g);
+  boost::adjacency_list<>::vertex_descriptor v3 = boost::add_vertex(g);
+  boost::adjacency_list<>::vertex_descriptor v4 = boost::add_vertex(g);
+
+  boost::add_edge(v1, v2, g);
+  boost::add_edge(v2, v3, g);
+  boost::add_edge(v2, v4, g);
+
+  boost::adjacency_list<>::adjacency_iterator vit, vend;
+  std::tie(vit, vend) = boost::adjacent_vertices(v2, g);
+
+  for(vit; vit != vend; vit++){
+    std::cout << *vit << std::endl;
+  }
 }
