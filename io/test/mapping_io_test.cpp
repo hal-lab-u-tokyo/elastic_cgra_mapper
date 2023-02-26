35b2e7ef444c6f61ef6b5926a9cd615f818165d7
msBRF65 <msaito6525@gmail.com>
2023-02-27 04:45:41 +0900

    add: mapping_io_test

---
+++ io/test/mapping_io_test.cpp
@@ -0,0 +1,71 @@
+#include <gtest/gtest.h>
+
+#include <io/mapping_io.hpp>
+#include <mapper/gurobi_mapper.hpp>
+
+TEST(IOTest, maping_io_test) {
+  // create mapping
+  // create dfg
+  std::vector<entity::Edge> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}};
+  int node_num = 6;
+  entity::DFGGraph g;
+  for (int i = 0; i < node_num; i++) {
+    auto v1 = boost::add_vertex(g);
+    g[v1].op = entity::OpType::ADD;
+  }
+
+  boost::graph_traits<entity::DFGGraph>::edge_descriptor e;
+  bool found;
+  for (auto edge : edges) {
+    boost::tie(e, found) = boost::add_edge(edge.first, edge.second, g);
+    g[e].operand = edge.first % 2;
+  }
+
+  entity::DFG dfg(g);
+  std::shared_ptr<entity::DFG> dfg_ptr = std::make_shared<entity::DFG>();
+  *dfg_ptr = dfg;
+
+  // create mrrg
+  entity::MRRGConfig mrrg_config;
+
+  mrrg_config.column = 4;
+  mrrg_config.row = 4;
+  mrrg_config.memory_io = entity::MRRGMemoryIOType::kAll;
+  mrrg_config.cgra_type = entity::MRRGCGRAType::kElastic;
+  mrrg_config.network_type = entity::MRRGNetworkType::kDiagonal;
+  mrrg_config.local_reg_size = 3;
+  mrrg_config.context_size = 2;
+
+  std::shared_ptr<entity::MRRG> mrrg_ptr = std::make_shared<entity::MRRG>();
+  *mrrg_ptr = entity::MRRG(mrrg_config);
+
+  // exec mapping
+  auto mapper_ptr = mapper::GurobiILPMapper().CreateMapper(dfg_ptr, mrrg_ptr);
+  entity::Mapping input_mapping;
+  bool is_succeed;
+  std::tie(is_succeed, input_mapping) = mapper_ptr->Execution();
+
+  EXPECT_EQ(is_succeed, true);
+
+  // write mapping
+  std::string file_name = "./mapping_data.json";
+  std::shared_ptr<entity::Mapping> input_mapping_ptr =
+      std::make_shared<entity::Mapping>();
+  *input_mapping_ptr = input_mapping;
+  io::WriteMappingFile(file_name, input_mapping_ptr, mrrg_config);
+
+  // read mapping
+  entity::Mapping output_mapping = io::ReadMappingFile(file_name);
+
+  // // mapping test
+  // entity::ConfigMap input_config_map, output_config_map;
+  // input_config_map = input_mapping.GetConfigMap();
+  // output_config_map = output_mapping.GetConfigMap();
+
+  // for (auto key_value : input_config_map) {
+  //   entity::CGRAConfig input_value = key_value.second;
+  //   entity::CGRAConfig output_value = output_config_map[key_value.first];
+
+  //   EXPECT_EQ(input_value.operation_type, output_value.operation_type);
+  // }
+}
