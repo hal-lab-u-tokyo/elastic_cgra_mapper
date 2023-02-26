4a38db2dbb2d6add68162a79f009adf52b641875
msBRF65 <msaito6525@gmail.com>
2023-02-27 04:44:51 +0900

    fix: mapping_io header

--- io/include/io/mapping_io.hpp
+++ io/include/io/mapping_io.hpp
@@ -5,5 +5,5 @@
 namespace io {
 entity::Mapping ReadMappingFile(std::string file_name);
 void WriteMappingFile(std::string file_name,
-                      std::shared_ptr<entity::Mapping> mapping_ptr_);
+                      std::shared_ptr<entity::Mapping> mapping_ptr_, entity::MRRGConfig mrrg_config);
 }  // namespace io
