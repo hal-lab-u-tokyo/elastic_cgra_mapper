df09718f6b2253e011584306ebe962d20c611832
msBRF65 <msaito6525@gmail.com>
2023-03-12 17:54:58 +0900

    fix: BFS in mapping constructor

--- entity/src/mapping.cpp
+++ entity/src/mapping.cpp
@@ -1,4 +1,5 @@
 #include <entity/mapping.hpp>
+#include <queue>
 
 entity::Mapping::Mapping(
     entity::MRRG mrrg, entity::DFG dfg,
@@ -23,54 +24,67 @@ entity::Mapping::Mapping(
       PE_id_vec.emplace_back(to_PE_id);
     }
 
-    const int kNopId = -1;
+    const int kRouteOpId = -1;
     std::vector<int> op_result_mrrg_reg_vec =
         dfg_output_to_mrrg_reg[from_op_id];
     for (int mrrg_reg_id : op_result_mrrg_reg_vec) {
-      PE_id_to_op_id_map.emplace(mrrg_reg_id, kNopId);
+      PE_id_to_op_id_map.emplace(mrrg_reg_id, kRouteOpId);
       PE_id_vec.emplace_back(mrrg_reg_id);
     }
 
     auto GetOpTypeFromPEId = [&](int PE_id) {
       int op_id = PE_id_to_op_id_map.at(PE_id);
-      if (op_id == kNopId) {
-        return entity::OpType::NOP;
+      if (op_id == kRouteOpId) {
+        return entity::OpType::ROUTE;
       }
       return dfg.GetNodeProperty(op_id).op;
     };
 
     auto GetOpNameFromPEId = [&](int PE_id) {
       int op_id = PE_id_to_op_id_map.at(PE_id);
-      if (op_id == kNopId) {
-        return std::string("");
+      if (op_id == kRouteOpId) {
+        return std::string("route");
       }
       return dfg.GetNodeProperty(op_id).op_name;
     };
 
-    for (int from_PE_id : PE_id_vec) {
+    // BFS
+    std::queue<int> from_PE_id_queue;
+    from_PE_id_queue.push(from_op_PE_id);
+    std::set<int> searched_PE_id = {from_op_PE_id};
+    while (from_PE_id_queue.size() > 0) {
+      int from_PE_id = from_PE_id_queue.front();
+      from_PE_id_queue.pop();
+
       std::vector<int> adj_PE_id_vec = mrrg.GetAdjacentNodeIdVec(from_PE_id);
       entity::ConfigId from_config_id(mrrg.GetNodeProperty(from_PE_id));
       entity::OpType from_op_type = GetOpTypeFromPEId(from_PE_id);
       std::string from_op_name = GetOpNameFromPEId(from_PE_id);
+
+      if (config_map_.count(from_config_id) == 0) {
+        config_map_.emplace(from_config_id,
+                            entity::CGRAConfig(from_op_type, from_op_name));
+      }
+
       for (int adj_PE_id : adj_PE_id_vec) {
+        if (searched_PE_id.count(adj_PE_id) > 0) continue;
         for (int to_PE_id : PE_id_vec) {
           if (adj_PE_id == to_PE_id) {
             entity::ConfigId to_config_id(mrrg.GetNodeProperty(to_PE_id));
             entity::OpType to_op_type = GetOpTypeFromPEId(to_PE_id);
             std::string to_op_name = GetOpNameFromPEId(to_PE_id);
 
-            if (config_map_.count(from_config_id) == 0) {
-              config_map_.emplace(
-                  from_config_id,
-                  entity::CGRAConfig::GenerateInitialCGRAConfig());
-            }
             if (config_map_.count(to_config_id) == 0) {
               config_map_.emplace(
                   to_config_id,
                   entity::CGRAConfig::GenerateInitialCGRAConfig());
             }
-            config_map_[from_config_id].AddToConfig(to_config_id, from_op_type, from_op_name);
-            config_map_[to_config_id].AddFromConfig(from_config_id, to_op_type, to_op_name);
+            config_map_[from_config_id].AddToConfig(to_config_id, from_op_type,
+                                                    from_op_name);
+            config_map_[to_config_id].AddFromConfig(from_config_id, to_op_type,
+                                                    to_op_name);
+            from_PE_id_queue.push(to_PE_id);
+            searched_PE_id.emplace(to_PE_id);
           }
         }
       }
