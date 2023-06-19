6bffc14d6a7042d3b37a6dd89808c56444a4e19a
msBRF65 <msaito6525@gmail.com>
2023-06-19 20:18:07 +0900

    refactor: verilog parameter

--- simulator/verilog/src/param.v
+++ simulator/verilog/src/param.v
@@ -1,16 +1,22 @@
 `ifndef PARAM
 `define PARAM
 
-parameter NEIGHBOR_PE_NUM = 4;
-parameter NEIGHBOR_PE_NUM_BIT_LENGTH = 2;
-parameter OPERATION_BIT_LENGTH = 4;
+// CGRA HW param
 parameter CONTEXT_SIZE_BIT_LENGTH = 3;  // log CONTEXT_HW_SIZE
 parameter CONTEXT_HW_SIZE = 8;
-parameter CONTEXT_SW_SIZE = 8;
-parameter DATA_WIDTH = 32;
-parameter CONFIG_DATA_BIT_LENGTH = NEIGHBOR_PE_NUM_BIT_LENGTH * 3 + OPERATION_BIT_LENGTH;
 parameter ADDRESS_WIDTH = 8;  // log MEMORY_SIZE
 parameter MEMORY_SIZE = 256;
 parameter CONTEXT_SWITCH_CLK_SIZE = 3;
+
+// CGRA data path param
+parameter DATA_WIDTH = 32;
+parameter OPERATION_BIT_LENGTH = 4;
+
+// CGRA topology param
+parameter NEIGHBOR_PE_NUM = 4;
+parameter NEIGHBOR_PE_NUM_BIT_LENGTH = 2;
+
+// CGRA execution param
+parameter CONTEXT_SW_SIZE = 8;
 
 `endif  // PARAM
