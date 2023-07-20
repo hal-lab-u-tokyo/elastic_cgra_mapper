80c3ff92add0f0ca826adf5824d6396109116c99
msBRF65 <msaito6525@gmail.com>
2023-07-20 17:43:47 +0900

    add: pe reg size bit length param

--- simulator/verilog/src/param.v
+++ simulator/verilog/src/param.v
@@ -22,6 +22,7 @@ parameter NEIGHBOR_PE_NUM_BIT_LENGTH = 2;
 parameter INPUT_NUM = 5;  // neighbor pe input + own output as input
 parameter INPUT_NUM_BIT_LENGTH = 3;
 parameter PE_REG_SIZE = 1;
+parameter PE_REG_SIZE_BIT_LENGTH = 1;
 
 // param for Elastic CGRA
 parameter ELASTIC_BUFFER_SIZE = 4;
