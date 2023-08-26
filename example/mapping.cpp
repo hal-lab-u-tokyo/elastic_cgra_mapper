884a8437a7aa524dcec3dd298f03c01b343bbc41
msBRF65 <msaito6525@gmail.com>
2023-08-26 20:26:41 +0900

    add: output log in mapping example

--- example/mapping.cpp
+++ example/mapping.cpp
@@ -7,7 +7,7 @@
 #include <mapper/gurobi_mapper.hpp>
 
 int main(int argc, char* argv[]) {
-  if (argc != 4) {
+  if (argc != 5) {
     std::cerr << "invalid arguments" << std::endl;
     abort();
   }
@@ -15,6 +15,7 @@ int main(int argc, char* argv[]) {
   std::string dfg_dot_file_path = argv[1];
   std::string mrrg_file_path = argv[2];
   std::string output_mapping_path = argv[3];
+  std::string log_file_dir = argv[4];
 
   std::shared_ptr<entity::DFG> dfg_ptr = std::make_shared<entity::DFG>();
   std::shared_ptr<entity::MRRG> mrrg_ptr = std::make_shared<entity::MRRG>();
@@ -22,8 +23,10 @@ int main(int argc, char* argv[]) {
   *dfg_ptr = io::ReadDFGDotFile(dfg_dot_file_path);
   *mrrg_ptr = io::ReadMRRGFromJsonFile(mrrg_file_path);
 
-  mapper::IILPMapper* mapper;
+  mapper::GurobiILPMapper* mapper;
   mapper = mapper::GurobiILPMapper().CreateMapper(dfg_ptr, mrrg_ptr);
+  mapper->SetLogFilePath(log_file_dir);
+
   std::shared_ptr<entity::Mapping> mapping_ptr =
       std::make_shared<entity::Mapping>();
   bool is_success = false;
@@ -31,4 +34,5 @@ int main(int argc, char* argv[]) {
 
   io::WriteMappingFile(output_mapping_path, mapping_ptr,
                        mrrg_ptr->GetMRRGConfig());
+
 }
