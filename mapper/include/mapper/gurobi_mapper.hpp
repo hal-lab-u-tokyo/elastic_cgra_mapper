06ff32f6e43188a6b6ee9b3ef81006d75b8c9a1c
msBRF65 <msaito6525@gmail.com>
2023-08-26 20:26:25 +0900

    add: output mapipng result to log file

--- mapper/include/mapper/gurobi_mapper.hpp
+++ mapper/include/mapper/gurobi_mapper.hpp
@@ -11,9 +11,11 @@ class GurobiILPMapper : public IILPMapper {
   GurobiILPMapper* CreateMapper(const std::shared_ptr<entity::DFG> dfg_ptr,
                                 const std::shared_ptr<entity::MRRG> mmrg_ptr);
   std::pair<bool, entity::Mapping> Execution();
+  void SetLogFilePath(const std::string& log_file_dir);
 
  private:
   std::shared_ptr<entity::DFG> dfg_ptr_;
   std::shared_ptr<entity::MRRG> mrrg_ptr_;
+  std::string log_file_path_;
 };
 }  // namespace mapper
