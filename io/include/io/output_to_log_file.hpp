f27dd93b9ea58043470b42a3b9230f4219897155
msBRF65 <msaito6525@gmail.com>
2023-12-22 15:07:55 +0000

    add: output log func

---
+++ io/include/io/output_to_log_file.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <entity/mapping.hpp>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <vector>
+
+namespace io {
+
+struct MappingInput {
+  std::filesystem::path dfg_dot_file_path;
+  entity::MRRGConfig mrrg_config;
+  std::filesystem::path output_dir_path;
+  double timeout_s;
+  int parallel_num;
+};
+struct MappingOutput {
+  double mapping_time_s;
+  bool is_success;
+  std::shared_ptr<entity::Mapping> mapping_ptr;
+  entity::MRRGConfig mrrg_config;
+};
+
+struct RemapperInput {
+  std::filesystem::path mapping_dir_path;
+  std::filesystem::path cgra_file_path;
+  std::filesystem::path output_dir_path;
+  std::string remapper_mode;
+};
+struct RemapperOutput {
+  double remapping_time_s;
+  std::shared_ptr<entity::Mapping> mapping_ptr;
+  entity::MRRGConfig mrrg_config;
+  int parallel_num;
+};
+
+struct CreateDatabaseInput {
+  std::filesystem::path dfg_dot_file_path;
+  std::filesystem::path cgra_file_path;
+  std::filesystem::path output_dir_path;
+  double db_timeout_s;
+  double min_utilization;
+};
+struct CreateDatabaseOutput {
+  double creating_db_time_s;
+};
+
+class Logger {
+ public:
+  Logger();
+  std::filesystem::path GetLogFilePath() const { return log_file_path_; };
+
+ protected:
+  void InitializePath(const std::filesystem::path& output_dir_path);
+  void CopyArchFile(const std::filesystem::path& original_arch_file_path);
+  std::filesystem::path output_dir_path_;
+  std::filesystem::path log_file_path_;
+  std::filesystem::path arch_file_path_;
+  std::ofstream log_file_;
+  time_t unixtime_;
+};
+
+class MappingLogger : public Logger {
+ public:
+  MappingLogger() : Logger() {}
+  void LogMappingInput(const MappingInput& input);
+  void LogMappingOutput(const MappingOutput& output);
+  std::string GetGurobiLogFilePath() const {
+    return gurobi_log_file_path_.string();
+  };
+
+ private:
+  std::filesystem::path gurobi_log_file_path_;
+  std::filesystem::path mapping_file_path_;
+};
+
+class RemapperLogger : public Logger {
+ public:
+  RemapperLogger() : Logger() {}
+  void LogRemapperInput(const RemapperInput& input);
+  void LogRemapperOutput(const RemapperOutput& output);
+  std::string GetRemapperExecLogFilePath() const {
+    return remapper_exec_log_file_path.string();
+  };
+
+ private:
+  std::filesystem::path mapping_file_path_;
+  std::filesystem::path remapper_exec_log_file_path;
+};
+
+class CreateDatabaseLogger : public Logger {
+ public:
+  CreateDatabaseLogger() : Logger() {}
+  void LogCreateDatabaseInput(const CreateDatabaseInput& input);
+  void LogCreateDatabaseOutput(const CreateDatabaseOutput& output);
+  std::string GetNextGurobiMappingPath(double mapping_timeout_s,
+                                       const entity::MRRGConfig& mrrg_config);
+  std::string GetSelectionLogFilePath() const {
+    return selection_log_file_path_.string();
+  };
+  void LogMapping(const MappingOutput& mapping_output);
+
+ private:
+  time_t mapping_unixtime_;
+  std::string database_id_;
+  CreateDatabaseInput input_;
+  MappingLogger mapping_logger_;
+  std::filesystem::path mapping_dir_path_;
+  std::filesystem::path selection_log_file_path_;
+  bool start_mapping_;
+};
+}  // namespace io
---
+++ io/src/output_to_log_file.cpp
@@ -0,0 +1,190 @@
+#include "io/output_to_log_file.hpp"
+
+#include "cassert"
+#include "io/architecture_io.hpp"
+#include "io/mapping_io.hpp"
+#include "time.h"
+
+io::Logger::Logger() {
+  unixtime_ = std::time(0);
+}
+
+void io::Logger::InitializePath(const std::filesystem::path& output_dir_path) {
+  assert(output_dir_path.is_absolute());
+  if (!std::filesystem::exists(output_dir_path)) {
+    std::filesystem::create_directories(output_dir_path);
+  };
+  output_dir_path_ = output_dir_path;
+}
+
+void io::Logger::CopyArchFile(
+    const std::filesystem::path& original_arch_file_path) {
+  assert(original_arch_file_path.is_absolute());
+  assert(std::filesystem::exists(original_arch_file_path));
+
+  std::filesystem::copy_file(original_arch_file_path, arch_file_path_,
+                             std::filesystem::copy_options::overwrite_existing);
+}
+
+void io::MappingLogger::LogMappingInput(const io::MappingInput& input) {
+  InitializePath(input.output_dir_path);
+  assert(input.dfg_dot_file_path.is_absolute());
+
+  log_file_path_ = output_dir_path_ / ("mapping/log/mapping_" +
+                                       std::to_string(unixtime_) + ".log");
+  arch_file_path_ = output_dir_path_ / ("mapping/cgra/cgra_" +
+                                        std::to_string(unixtime_) + ".json");
+  mapping_file_path_ = output_dir_path_ / ("mapping/mapping/mapping_" +
+                                           std::to_string(unixtime_) + ".json");
+  gurobi_log_file_path_ =
+      output_dir_path_ /
+      ("mapping/gurobi_log/gurobi_" + std::to_string(unixtime_) + ".log");
+
+  std::shared_ptr<entity::MRRG> mrrg_ptr =
+      std::make_shared<entity::MRRG>(input.mrrg_config);
+  io::WriteMRRGToJsonFile(arch_file_path_.string(), mrrg_ptr);
+
+  log_file_.open(log_file_path_, std::ios::app);
+  log_file_ << "-- mapping input --" << std::endl;
+  log_file_ << "dfg file: " << input.dfg_dot_file_path.string() << std::endl;
+  log_file_ << "cgra file: " << arch_file_path_.string() << std::endl;
+  log_file_ << "output dir: " << output_dir_path_.string() << std::endl;
+  log_file_ << "timeout (s): " << input.timeout_s << std::endl;
+  log_file_ << "parallel num: " << input.parallel_num << std::endl;
+}
+
+void io::MappingLogger::LogMappingOutput(const io::MappingOutput& output) {
+  io::WriteMappingFile(mapping_file_path_, output.mapping_ptr,
+                       output.mrrg_config);
+  log_file_ << "-- mapping output --" << std::endl;
+  log_file_ << "mapping time (s): " << output.mapping_time_s << std::endl;
+  log_file_ << "is success: " << output.is_success << std::endl;
+  log_file_ << "mapping file: " << mapping_file_path_.string() << std::endl;
+  log_file_ << "gurobi log file: " << gurobi_log_file_path_.string()
+            << std::endl;
+  log_file_.close();
+}
+
+void io::RemapperLogger::LogRemapperInput(const io::RemapperInput& input) {
+  InitializePath(input.output_dir_path);
+  assert(input.mapping_dir_path.is_absolute());
+  assert(input.cgra_file_path.is_absolute());
+  assert(input.remapper_mode == "dp" || input.remapper_mode == "greedy" ||
+         input.remapper_mode == "full_search");
+
+  log_file_path_ = output_dir_path_ /
+                   ("remapping/" + input.remapper_mode + "/log/remapping_" +
+                    std::to_string(unixtime_) + ".log");
+  arch_file_path_ =
+      output_dir_path_ / ("remapping/" + input.remapper_mode + "/cgra/cgra_" +
+                          std::to_string(unixtime_) + ".json");
+  mapping_file_path_ = output_dir_path_ / ("remapping/" + input.remapper_mode +
+                                           "/mapping/remapping_" +
+                                           std::to_string(unixtime_) + ".json");
+  remapper_exec_log_file_path =
+      output_dir_path_ /
+      ("remapping/" + input.remapper_mode + "/exec_log/exec_log_" +
+       std::to_string(unixtime_) + ".log");
+
+  CopyArchFile(input.cgra_file_path);
+
+  log_file_.open(log_file_path_, std::ios::app);
+  log_file_ << "-- remapping input --" << std::endl;
+  log_file_ << "mapping dir: " << input.mapping_dir_path.string() << std::endl;
+  log_file_ << "cgra file: " << arch_file_path_.string() << std::endl;
+  log_file_ << "output dir: " << output_dir_path_.string() << std::endl;
+  log_file_ << "remapper mode: " << input.remapper_mode << std::endl;
+}
+
+void io::RemapperLogger::LogRemapperOutput(const io::RemapperOutput& output) {
+  assert(output.remapping_time_s > 0);
+  io::WriteMappingFile(mapping_file_path_, output.mapping_ptr,
+                       output.mrrg_config);
+  log_file_ << "-- remapping output --" << std::endl;
+  log_file_ << "remapping time (s): " << output.remapping_time_s << std::endl;
+  log_file_ << "parallel num: " << output.parallel_num << std::endl;
+  log_file_ << "mapping file: " << mapping_file_path_.string() << std::endl;
+  log_file_.close();
+}
+
+std::string GetCGRAId(const std::filesystem::path& cgra_file_path) {
+  const auto mrrg_config =
+      io::ReadMRRGFromJsonFile(cgra_file_path).GetMRRGConfig();
+
+  std::string cgra_id = "";
+  cgra_id += "c" + std::to_string(mrrg_config.column);
+  cgra_id += "r" + std::to_string(mrrg_config.row);
+  cgra_id += "_" + entity::MRRGMemoryIoTypeToString(mrrg_config.memory_io);
+  cgra_id += "_" + entity::MRRGCGRATypeToString(mrrg_config.cgra_type);
+  cgra_id += "_" + entity::MRRGNetworkTypeToString(mrrg_config.network_type);
+  cgra_id += "_l" + std::to_string(mrrg_config.local_reg_size);
+  cgra_id += "x" + std::to_string(mrrg_config.context_size);
+
+  return cgra_id;
+}
+
+void io::CreateDatabaseLogger::LogCreateDatabaseInput(
+    const io::CreateDatabaseInput& input) {
+  InitializePath(input.output_dir_path);
+  assert(input.dfg_dot_file_path.is_absolute());
+  assert(input.cgra_file_path.is_absolute());
+
+  input_ = input;
+  log_file_path_ = output_dir_path_ /
+                   ("database/log/db_" + std::to_string(unixtime_) + ".log");
+  arch_file_path_ = output_dir_path_ / ("database/cgra/cgra_" +
+                                        std::to_string(unixtime_) + ".json");
+
+  CopyArchFile(input.cgra_file_path);
+
+  database_id_ = GetCGRAId(input.cgra_file_path) + "_" +
+                 std::to_string(static_cast<int>(input.db_timeout_s));
+  selection_log_file_path_ =
+      output_dir_path_ / ("database/selection_log/selection_log_" +
+                          std::to_string(unixtime_) + ".log");
+
+  log_file_.open(log_file_path_, std::ios::app);
+  log_file_ << "-- create database input --" << std::endl;
+  log_file_ << "dfg file: " << input.dfg_dot_file_path.string() << std::endl;
+  log_file_ << "cgra file: " << arch_file_path_.string() << std::endl;
+  log_file_ << "output dir: " << output_dir_path_.string() << std::endl;
+  log_file_ << "timeout (s): " << input.db_timeout_s << std::endl;
+  log_file_ << "min utilization: " << input.min_utilization << std::endl;
+}
+
+void io::CreateDatabaseLogger::LogCreateDatabaseOutput(
+    const io::CreateDatabaseOutput& output) {
+  log_file_ << "-- create database output --" << std::endl;
+  log_file_ << "creating db time (s): " << output.creating_db_time_s
+            << std::endl;
+  log_file_.close();
+}
+
+std::string io::CreateDatabaseLogger::GetNextGurobiMappingPath(
+    double mapping_timeout_s, const entity::MRRGConfig& mrrg_config) {
+  start_mapping_ = true;
+  MappingInput mapping_input;
+  mapping_input.mrrg_config = mrrg_config;
+  mapping_input.dfg_dot_file_path = input_.dfg_dot_file_path;
+  mapping_input.output_dir_path =
+      output_dir_path_ / ("database/mapping/" + database_id_);
+  mapping_input.timeout_s = mapping_timeout_s;
+  mapping_input.parallel_num = 1;
+  mapping_logger_.LogMappingInput(mapping_input);
+
+  return output_dir_path_ /
+         ("database/mapping/" + database_id_ + "/mapping/gurobi_log/gurobi_" +
+          std::to_string(mapping_unixtime_) + ".log");
+}
+
+void io::CreateDatabaseLogger::LogMapping(
+    const io::MappingOutput& mapping_output) {
+  assert(start_mapping_);
+
+  mapping_logger_.LogMappingOutput(mapping_output);
+
+  log_file_ << "mapping_log_file:" << mapping_logger_.GetLogFilePath().string()
+            << std::endl;
+
+  start_mapping_ = false;
+}
