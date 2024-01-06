d9da5c7d236895cf3e44fd793c1b1e4be98ec201
msBRF65 <msaito6525@gmail.com>
2024-01-06 16:11:52 +0000

    add: eq operator to cgra

--- python_tools/entity/cgra.py
+++ python_tools/entity/cgra.py
@@ -10,6 +10,24 @@ class CGRA:
     self.local_reg_size = local_reg_size
     self.context_size = context_size
 
+  def __eq__(self, other):
+    if self.row != other.row:
+      return False
+    if self.column != other.column:
+      return False
+    if self.memory_io_type != other.memory_io_type:
+      return False
+    if self.cgra_type != other.cgra_type:
+      return False
+    if self.network_type != other.network_type:
+      return False
+    if self.local_reg_size != other.local_reg_size:
+      return False
+    if self.context_size != other.context_size:
+      return False
+    return True
+
+
   def get_cgra_dict(self):
     cgra_dict = {}
     cgra_dict["row"] = self.row
