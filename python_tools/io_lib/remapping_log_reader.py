45e15c82969f52a5cca0bd331762930db73edec4
msBRF65 <msaito6525@gmail.com>
2024-02-09 05:50:51 +0000

    fix: remapping log reader

--- python_tools/io_lib/remapping_log_reader.py
+++ python_tools/io_lib/remapping_log_reader.py
@@ -31,15 +31,17 @@ def remapping_log_reader(log_file_path, benchmark_list=[]) -> Tuple[bool, Remapp
     with open(log_file_path) as f:
         read_mode = False
         line_num = 1
-        mapping_file_num = 100000000
+        offset_init = 1000000000000
+        offset = offset_init
         for line in f:
             if line_num >= 3:
                 parsed = parse.parse(">> mapping file: {}\n", line)
                 if parsed == None:
-                    mapping_file_num = len(remapper_log_info.mapping_json_list)
+                    if offset == offset_init:
+                        offset = len(remapper_log_info.mapping_json_list)
                 else:
                     remapper_log_info.mapping_json_list.append(parsed[0])
-            if line_num == mapping_file_num + 3:
+            if line_num == offset + 3:
                 cgra_file = parse.parse("cgra file: {}\n", line)[0]
                 cgra = cgra_json_reader(cgra_file)
                 remapper_log_info.row = cgra.row
@@ -49,27 +51,32 @@ def remapping_log_reader(log_file_path, benchmark_list=[]) -> Tuple[bool, Remapp
                 remapper_log_info.cgra_type = cgra.cgra_type
                 remapper_log_info.network_type = cgra.network_type
                 remapper_log_info.local_reg_size = cgra.local_reg_size
-            if line_num == mapping_file_num + 5:
+            if line_num == offset + 5:
                 mode = parse.parse("remapper mode: {:w}\n", line)[0]
-                remapper_log_info.remapper_mode = RemapperType.from_string(mode)            
-            if line_num == mapping_file_num + 7:
+                remapper_log_info.remapper_mode = RemapperType.from_string(mode)
+            if line_num == offset + 6:
+                parsed = parse.parse("timeout_s: {:d}\n", line)
+                if parsed != None:
+                    remapper_timeout_s = parsed[0]            
+                    offset = offset + 1                    
+            if line_num == offset + 7:
                 remapping_time = float(parse.parse("remapping time (s): {}\n", line)[0])
                 remapper_log_info.remapper_time = remapping_time
-            if line_num == mapping_file_num + 8:
+            if line_num == offset + 8:
                 parallel_num_from_exec = get_parallel_num(log_file_path)
                 parallel_num = parse.parse("parallel num: {:d}\n", line)[0]
-                remapper_log_info.parallel_num = min(parallel_num, parallel_num_from_exec) # some log file has upper limit of parallel num instead of the result of remapping
+                remapper_log_info.parallel_num = parallel_num # some log file has upper limit of parallel num instead of the result of remapping
 
                 if remapper_log_info.parallel_num > 0:
                     remapper_log_info.mapping_succeed = True
-            if line_num == mapping_file_num + 9:
+            if line_num == offset + 9:
                 parsed = parse.parse("mapping type num: {:d}\n", line)
                 if parsed != None:
                     remapper_log_info.mapping_type_num = parsed[0]
 
             line_num = line_num + 1
 
-    if line_num <= mapping_file_num + 8:
+    if line_num <= offset + 8:
         return (False, remapper_log_info)
 
     return (True, remapper_log_info)
