a16935d9072865687bedb4485e0e6c17f8b52491
msBRF65 <msaito6525@gmail.com>
2026-03-11 12:12:34 +0000

    add: operations

--- entity/include/entity/operation.hpp
+++ entity/include/entity/operation.hpp
@@ -6,9 +6,13 @@
 namespace entity {
 enum OpType {
   ADD,
+  FADD,
   SUB,
   MUL,
+  FMUL,
   DIV,
+  SDIV,
+  FDIV,
   CONST,
   LOAD,
   OUTPUT,
@@ -16,7 +20,14 @@ enum OpType {
   NOP,
   ROUTE,
   OR,
+  SHIFT,
+  ICMP,
+  CMPGT,
+  CMPGE,
+  CMPEQ,
+  FSUB,
   LOOP,
+  SELECT
 };
 std::string OpTypeToString(OpType op);
 OpType OpTypeFromString(std::string op_string);
